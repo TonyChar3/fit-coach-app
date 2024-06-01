115c3d3f2c434c682024bcc5f3a8cca6420b3bac
Tony Char3 <tonyc6731@gmail.com>
2024-06-01 10:06:54 -0400

    UPDATE: The theme for the app is all set up, will change the colors scheme but everything is set up to start developping the MVP

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts as useSourcePro,
+  SourceSansPro_400Regular,
+  SourceSansPro_700Bold,
+  SourceSansPro_300Light
+} from "@expo-google-fonts/source-sans-pro";
 
 export default function App() {
+  const [sourceSansPro] = useSourcePro({
+    SourceSansPro_400Regular,
+    SourceSansPro_700Bold,
+    SourceSansPro_300Light
+  });
+
+  if (!sourceSansPro) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,6 +31,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    fontFamily: "SourceSansPro_700Bold",
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/components/utility/safe-area.component.js
@@ -0,0 +1,12 @@
+import { StatusBar, SafeAreaView, Platform } from "react-native";
+import styled from "styled-components/native";
+
+const statusBarHeight = Platform.OS === "android" ? StatusBar.currentHeight : 0;
+
+export const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  margin-top: ${statusBarHeight}px;
+  background-color: white;
+`;
+
+// ${(props) => props.theme.colors.bg.primary}
